e2b3a11e30108514a2c11a5b05e021cc6a6b8609
Al-va-rez <167593484+Al-va-rez@users.noreply.github.com>
2025-05-24 16:15:39 +0800

    Add files via upload

---
+++ mmd_Finals_Exam/workspace/workspace_Scripts.js
@@ -0,0 +1,35 @@
+let myTimeout;
+$('#editor').on('input',
+    function(event) {
+        event.preventDefault();
+
+        clearTimeout(myTimeout);
+
+        var formData = {
+            content: $(this).html(),
+            docId: $(this).data('doc-id'),
+            saveReq: 1
+        };
+
+        myTimeout = setTimeout(function() {
+            $.ajax(
+                {
+                    type: 'POST',
+                    url: '../core/handleForms.php',
+                    data: formData,
+                    success: function(data) {
+                        switch (data) {
+                            case 'Success':
+                                console.log(formData.content);
+                                break;
+                        
+                            default:
+                                console.log('aaaaaaaaaaaaaaaa');
+                                break;
+                        }
+                    }
+                }
+            )
+        }, 1000);
+    }
+);
