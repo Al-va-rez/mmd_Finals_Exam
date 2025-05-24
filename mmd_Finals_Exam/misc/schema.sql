ff5ff96514db608fb89c5e43a59763895f4b8d45
Al-va-rez <167593484+Al-va-rez@users.noreply.github.com>
2025-05-24 14:57:54 +0800

    Delete mmd_Finals_Exam directory

--- mmd_Finals_Exam/misc/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE user_credentials (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    role ENUM('User', 'Admin') NOT NULL,
-    username VARCHAR (50),
-    first_Name VARCHAR (50),
-    last_Name VARCHAR (50),
-    password text,
-    date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
